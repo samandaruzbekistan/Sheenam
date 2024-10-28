060a87c8c2c4d722c213ef84c9488f91a4eac416
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2024-10-28 21:26:39 +0500

    CONFIG: Register Broker

--- Sheenam.Api/Program.cs
+++ Sheenam.Api/Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StorageBroker>();
+builder.Services.AddTransient<IStorageBroker, StorageBroker>();
 
 var app = builder.Build();
 
