4fa4982405c92d3932ba43be59df649fe1339da3
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2024-10-31 09:37:00 +0500

    FOUNDATIONS: Guest Add And Tests

---
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.cs
@@ -0,0 +1,20 @@
+﻿using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Services.Foundations.Guests
+{
+    public class GuestService : IGuestService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public GuestService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public ValueTask<Guest> AddGuestAsync(Guest guest)
+        {
+            return this.storageBroker.InsertGuestAsync(guest);
+        }
+    }
+}
---
+++ Sheenam.Api/Services/Foundations/Guests/IGuestService.cs
@@ -0,0 +1,11 @@
+﻿using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Services.Foundations.Guests
+{
+    public interface IGuestService
+    {
+        ValueTask<Guest> AddGuestAsync(Guest guest);
+
+
+    }
+}
